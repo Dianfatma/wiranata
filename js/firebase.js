a72c3bcbad5e5906744d7685da668984339f17bc
Emma Paris <eparis@atlassian.com>
2019-10-21 12:28:48 +0700

    update

---
+++ js/firebase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyAGAkHGiIFQyuZOwmdgoYdub_0lDMfxqgE",
+  authDomain: "sysca-2aac7.firebaseapp.com",
+  databaseURL: "https://sysca-2aac7.firebaseio.com",
+  projectId: "sysca-2aac7",
+  storageBucket: "sysca-2aac7.appspot.com",
+  messagingSenderId: "586767718818",
+  appId: "1:586767718818:web:7d4aae826e4fd146"
+};
+
+  firebase.initializeApp(firebaseConfig);
